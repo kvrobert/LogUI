f2ff55c86de049db69a92c176f4056cf93a14506
Robesz <kvrobert@gmail.com>
2017-12-01 06:42:31 +0100

    UI try

---
+++ public_html/index.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var text = "Some text";
+
+alert(text.search("ex"));
+
+var arr = [];
+
+arr.
